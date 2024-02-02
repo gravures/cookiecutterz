d5842ff8f1d4d7835c4645f8cd831522b159575e
Gilles Coissac <gilles@ideographe.fr>
2024-02-02 13:59:45 +0100

    adding the cruft wrapper...

---
+++ src/cookiecutterz/cruft.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2023 - Gilles Coissac
+# See end of file for extended copyright information
+from __future__ import annotations
+
+from cruft._cli import app
+
+import cookiecutterz  # noqa: F401
+
+
+def main():
+    app()
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Copyright (c) 2023 - Gilles Coissac
+# This file is part of Cookicutterz program.
+#
+# Cookiecutterz is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# Cookiecutterz is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cookiecutterz. If not, see <https://www.gnu.org/licenses/>
